3fb60fdbd334d86de14d61ab38f68d57b19f5f4e
chan000518 <wkdvnddl050701@gmail.com>
2024-11-13 03:53:02 +0900

    chap 1.2

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
-import express from "express"
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
 
 const app = express();
 
-// view엔진 설정
 app.set("view engine", "pug");
-//view디렉토리 설정
 app.set("views", __dirname + "/views");
-// use를 통해 /public디렉토리에 있는 파일들 전달
 app.use("/public", express.static(__dirname + "/public"));
-// 홈으로 랜더(뷰 엔진)
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-// 이건 왜 따로 뺀지 몰루?
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+server.listen(3000, handleListen);
