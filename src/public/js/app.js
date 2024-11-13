f52ab8f2daa2488852356d1d1a3d7e1f900616b6
chan000518 <wkdvnddl050701@gmail.com>
2024-11-13 15:21:58 +0900

    chap 1.5

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,24 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✅");
-});
-socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
-});
-socket.addEventListener("close", () => {
+function handleOpen() {
+    console.log("Connected to Server ✅");   
+}
+
+// 서버가 보낸 메세지를 받아서 처리
+function handleMessage(message) {
+    console.log("New message: ", message.data)
+}
+
+function handleClose() {
     console.log("Disconnected from Server ❌");
-});
+}
+
+socket.addEventListener("open", handleOpen);
+socket.addEventListener("message", handleMessage);
+socket.addEventListener("close", handleClose);
+
 setTimeout(() => {
+    // 서버로 메세지를 보냄
     socket.send("hello from the browser!");
 }, 10000);
   
--- src/server.js
+++ src/server.js
@@ -14,18 +14,27 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// 연결이 끝나면 실행 (이벤트: close)
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+// 메세지가 오면 실행
+function onSocketMessage(message) {
+    console.log(message);
+    // 문자열로 변환해 출력
+    console.log(String(message));
+}
+  
 // 연결이 되면 실행 (이벤트:connection)
 // handleConnection 콜백함수(웹소켓, 인커밋 메세지) 실행
 wss.on("connection", (socket) => {
     console.log("Connected to Browser ✅");
-    // 연결이 끝나면 실행 (이벤트: close)
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    
+    socket.on("close", onSocketClose);
     // 메세지가 오면 메세지 출력
-    // 버퍼가 출력, 
-    socket.on("message", (message) => {
-        console.log(message);
-        console.log(String(message));
-    });
+    // 버퍼가 출력(아스키 코드로)
+    socket.on("message", onSocketMessage);
     socket.send("hello!!!");
 });
 
