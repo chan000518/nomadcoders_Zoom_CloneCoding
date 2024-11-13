f78e5d70e926bf3d603cf61c538ba377c3eea3a2
chan000518 <wkdvnddl050701@gmail.com>
2024-11-13 15:12:47 +0900

    chap 1.4

--- src/server.js
+++ src/server.js
@@ -14,12 +14,19 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// 전달 받은 소켓 출력
-function handleConnection(socket) {
-    console.log(socket);
-}
-// 연결이 되면 실행 이벤트:connection
+// 연결이 되면 실행 (이벤트:connection)
 // handleConnection 콜백함수(웹소켓, 인커밋 메세지) 실행
-wss.on("connection", handleConnection);
-  
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+    // 연결이 끝나면 실행 (이벤트: close)
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    // 메세지가 오면 메세지 출력
+    // 버퍼가 출력, 
+    socket.on("message", (message) => {
+        console.log(message);
+        console.log(String(message));
+    });
+    socket.send("hello!!!");
+});
+
 server.listen(3000, handleListen);
