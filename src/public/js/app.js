d4dc4cd421bc3a2289b8c66e85a02a1a34399ceb
chan000518 <wkdvnddl050701@gmail.com>
2024-11-13 15:39:50 +0900

    chap 1.6

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,7 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("form");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 function handleOpen() {
@@ -17,8 +21,19 @@ socket.addEventListener("open", handleOpen);
 socket.addEventListener("message", handleMessage);
 socket.addEventListener("close", handleClose);
 
-setTimeout(() => {
-    // 서버로 메세지를 보냄
-    socket.send("hello from the browser!");
-}, 10000);
-  
+// setTimeout(() => {
+//     // 서버로 메세지를 보냄
+//     socket.send("hello from the browser!");
+// }, 10000);
+
+// 제출한 값을 소켓으로 전달
+function handleSubmit(event) {
+    // 폼이 제출되는 기본 동작(즉, 페이지 새로고침)을 막음
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+// 버튼을 누르면 실행
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -25,17 +25,31 @@ function onSocketMessage(message) {
     // 문자열로 변환해 출력
     console.log(String(message));
 }
+
+// 소켓들을 관리할 배열
+const sockets = [];
   
 // 연결이 되면 실행 (이벤트:connection)
 // handleConnection 콜백함수(웹소켓, 인커밋 메세지) 실행
 wss.on("connection", (socket) => {
+    // 소켓
+    sockets.push(socket);
+
     console.log("Connected to Browser ✅");
     
     socket.on("close", onSocketClose);
     // 메세지가 오면 메세지 출력
     // 버퍼가 출력(아스키 코드로)
     socket.on("message", onSocketMessage);
+    
+    // 연결이 되면 hello 전송
     socket.send("hello!!!");
+    
+    // 소켓 여러개를 관리 소켓을 연결할때 메세지 이벤트 on("message")로 등록하여
+    // 하나의 소켓에서 받은 메세지를 연결된 모든 소켓에 전달
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => aSocket.send(message));
+    });
 });
 
 server.listen(3000, handleListen);
